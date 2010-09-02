2c992a5168c8a65b6ebc534d7382e0093264af7d
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-09-02 14:13:30 -0400

    Use custom loader preferably
    The custom loaders are now inserted before the default path variable related
    loaders. This is done mainly to have automated file watching if provided by
    the surrounding application.

--- src/roslua/init.lua
+++ src/roslua/init.lua
@@ -22,8 +22,8 @@ local utils = require("roslua.utils")
 
 -- Add our custom loader, we do this outside of init to avoid having to call
 -- init before loading required modules
-table.insert(package.loaders, 4, utils.package_loader)
-table.insert(package.loaders, 5, utils.c_package_loader)
+table.insert(package.loaders, 2, utils.package_loader)
+table.insert(package.loaders, 4, utils.c_package_loader)
 
 require("roslua.master_proxy")
 require("roslua.param_proxy")
