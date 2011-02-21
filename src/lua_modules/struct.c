f455178cd59c631e9a53b389bcd41d391ee3e4e0
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2011-02-21 15:05:37 +0100

    lua_modules: added struct.unpack_table.
    Added an unpack function which returns a table of values rather than all
    values. Especially in the case you have a large or unknown number of
    values this is useful.
    
    It is also required to fix a potential race condition with the Lua
    garbage collector. Consder this code:
    {{{
    local t = {struct.unpack(format, binstring)}.
    }}}
    This will assign a table to t containing all read values. This seems to
    be a weak table (so I didn't trace the particular piece of code where
    this is decided). If the table is large, it might be that the garbage
    collector has erased its values before one is done with it, causing a
    segfault or glibc double linked list corruption warning.

--- src/lua_modules/struct.c
+++ src/lua_modules/struct.c
@@ -390,6 +390,94 @@ static int b_unpack (lua_State *L) {
   return lua_gettop(L) - 2;
 }
 
+
+static int b_unpack_table (lua_State *L) {
+  Header h;
+  const char *fmt = luaL_checkstring(L, 1);
+  size_t ld;
+  const char *data = luaL_checklstring(L, 2, &ld);
+  size_t pos = luaL_optinteger(L, 3, 1) - 1;
+  defaultoptions(&h);
+  lua_settop(L, 2);
+
+  int arrind = 0;
+
+  lua_newtable(L);
+
+  while (*fmt) {
+    int opt = *fmt++;
+    size_t size = optsize(L, opt, &fmt);
+    pos += gettoalign(pos, &h, opt, size);
+    luaL_argcheck(L, pos+size <= ld, 2, "data string too short");
+    switch (opt) {
+      case 'b': case 'B': case 'h': case 'H':
+      case 'l': case 'L': case 'i':  case 'I': {  /* integer types */
+        int issigned = islower(opt);
+        lua_Number res = getinteger(data+pos, h.endian, issigned, size);
+	lua_pushinteger(L, ++arrind);
+        lua_pushnumber(L, res);
+	lua_settable(L, 3);
+        break;
+      }
+      case 'x': {
+        break;
+      }
+      case 'f': {
+        float f;
+        memcpy(&f, data+pos, size);
+        correctbytes((char *)&f, sizeof(f), h.endian);
+	lua_pushinteger(L, ++arrind);
+        lua_pushnumber(L, f);
+	lua_settable(L, 3);
+        break;
+      }
+      case 'd': {
+        double d;
+        memcpy(&d, data+pos, size);
+        correctbytes((char *)&d, sizeof(d), h.endian);
+	lua_pushinteger(L, ++arrind);
+        lua_pushnumber(L, d);
+	lua_settable(L, 3);
+        break;
+      }
+      case 'c': {
+        if (size == 0) {
+	  lua_pushinteger(L, arrind);
+	  lua_gettable(L, 3);
+          if (!lua_isnumber(L, -1))
+            luaL_error(L, "format `c0' needs a previous size");
+          size = lua_tonumber(L, -1);
+          lua_pop(L, 1);
+          luaL_argcheck(L, pos+size <= ld, 2, "data string too short");
+	  lua_pushinteger(L, arrind--);
+	  lua_pushnil(L);
+	  lua_settable(L, 3);
+        }
+	lua_pushinteger(L, ++arrind);
+        lua_pushlstring(L, data+pos, size);
+	lua_settable(L, 3);
+        break;
+      }
+      case 's': {
+        const char *e = (const char *)memchr(data+pos, '\0', ld - pos);
+        if (e == NULL)
+          luaL_error(L, "unfinished string in data");
+        size = (e - (data+pos)) + 1;
+	lua_pushinteger(L, ++arrind);
+        lua_pushlstring(L, data+pos, size - 1);
+	lua_settable(L, 3);
+        break;
+      }
+      default: commoncases(L, opt, &fmt, &h);
+    }
+    pos += size;
+  }
+  lua_pushinteger(L, ++arrind);
+  lua_pushinteger(L, pos + 1);
+  lua_settable(L, 3);
+  return lua_gettop(L) - 2;
+}
+
 /* }====================================================== */
 
 
@@ -397,6 +485,7 @@ static int b_unpack (lua_State *L) {
 static const struct luaL_reg thislib[] = {
   {"pack", b_pack},
   {"unpack", b_unpack},
+  {"unpack_table", b_unpack_table},
   {"size", b_size},
   {NULL, NULL}
 };
